3932c235a7ff6efce4ede1786ab1474429702e08
Yuvraj-kumar00 <yuvrajkumar91174@gmail.com>
2025-02-19 18:11:54 +0530

    Delete REVIEW-COLLECTOR-PROJECT/app2.js

--- REVIEW-COLLECTOR-PROJECT/app2.js
+++
@@ -1,6 +0,0 @@
-const themeMode = document.querySelector("#theme-mode");
-const body = document.querySelector("body");
-
-themeMode.addEventListener("click", () => {
-    body.classList.toggle("dark-mode")
-})
